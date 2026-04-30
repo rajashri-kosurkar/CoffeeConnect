05baef9dbfc3d26c1b853ebb1a659c51867f03fe
Rajashri Kosurkar <rajashri.kosurkar@gmail.com>
2026-04-30 13:51:51 +0100

    Initial Commit

---
+++ CoffeeConnect/CoffeeConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeConnectApp.swift
+//  CoffeeConnect
+//
+//  Created by Rajshri Kosurkar on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
