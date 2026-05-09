c47e4e12e572f58170f07ba0244f759cb735bbfd
Rajashri Kosurkar <rajashri.kosurkar@gmail.com>
2026-05-09 13:38:23 +0100

    [Feature] Implemented CoffeeBeanGridRowView and implemented gridContentView to display the grid

---
+++ CoffeeConnect/CoffeeBeans/View/CoffeeBeanGridRowView.swift
@@ -0,0 +1,67 @@
+//
+//  CoffeeBeanGridRowView.swift
+//  CoffeeConnect
+//
+//  Created by Rajshri Kosurkar on 09/05/26.
+//
+
+import SwiftUI
+
+struct CoffeeBeanGridRowView: View {
+    let coffeeBean: CoffeeBean
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            beanBackgroundImage
+            beanDetails
+        }
+        .background(Color(.secondarySystemBackground))
+        .clipShape(RoundedRectangle(cornerRadius: 15))
+    }
+}
+
+extension CoffeeBeanGridRowView {
+    
+    // MARK: Subviews
+
+    private var beanBackgroundImage: some View {
+        
+        ZStack(alignment: .topTrailing) {
+            AsyncImage(url: URL(string: coffeeBean.imageURL)) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+            } placeholder: {
+                Color.brown.opacity(0.8)
+            }
+            .frame(height: 120)
+            .clipped()
+            
+            if coffeeBean.isBOTD {
+                BOTDBadge()
+                    .padding(10)
+            }
+        }
+    }
+    
+    private var beanDetails: some View {
+        VStack(alignment: .leading) {
+            Text(coffeeBean.name)
+                    .font(.headline)
+                    .lineLimit(1)
+            Text(coffeeBean.country)
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+            
+            Text(coffeeBean.cost)
+                .font(.subheadline.weight(.semibold))
+                .foregroundStyle(.primary)
+        }
+        .padding(10)
+    }
+}
+
+
+#Preview {
+    CoffeeBeanGridRowView(coffeeBean: CoffeeBean.dummy)
+}
--- CoffeeConnect/CoffeeBeans/View/CoffeeBeanListView.swift
+++ CoffeeConnect/CoffeeBeans/View/CoffeeBeanListView.swift
@@ -11,6 +11,12 @@ struct CoffeeBeanListView: View {
     
     @Environment(CoffeeBeanListViewModel.self) var viewModel
     
+    // Grid layout columns
+    private let gridColumns: [GridItem] = [
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible(), spacing: 16)
+    ]
+    
     var body: some View {
         
         NavigationStack {
@@ -104,8 +110,22 @@ extension CoffeeBeanListView {
         }
         .padding(.top, 8)
     }
+    
     private var gridContentView: some View {
-        Text("Grid content")
+        LazyVGrid(columns: gridColumns, spacing: 16) {
+            
+            ForEach(viewModel.coffeeBeans) { bean in
+                NavigationLink {
+                    CoffeeBeanDetailView(coffeeBean: bean)
+                } label: {
+                    CoffeeBeanGridRowView(coffeeBean: bean)
+                }
+                .buttonStyle(.plain)
+                
+            }
+        }
+        .padding(.horizontal, 16)
+        .padding(.top, 8)
     }
 }
 
